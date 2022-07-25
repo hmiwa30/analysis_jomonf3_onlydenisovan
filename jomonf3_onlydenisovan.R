ca093a01d2efb47eb5a5ebe8813c5ba9df4c5265
hmiwa30 <109275358+hmiwa30@users.noreply.github.com>
2022-07-25 17:50:00 +0900

    Create jomonf3_onlydenisovan.R

---
+++ jomonf3_onlydenisovan.R
@@ -0,0 +1,89 @@
+pops <- read.csv("1000G.p3.name.csv",header=F)
+
+test_sum <- as.vector(pops$V1[-c(1)])
+test_sum <- append(test_sum,c("Jomon","IK002.maf001_Jomon","JpFu1","JpKo13","JpHi01","JpKo2","JpIw31","JpOd181","JpIw32","JpOd274","JpIw33","JpOd282","JpKa6904","JpOd6"))
+test_pop <- as.vector(pops$V7[-c(1)])
+test_pop <- append(test_pop,c("Jomon","IK002.maf001_Jomon","JpFu1","JpKo13","JpHi01","JpKo2","JpIw31","JpOd181","JpIw32","JpOd274","JpIw33","JpOd282","JpKa6904","JpOd6"))
+df1 <- data.frame(v1=test_sum,v2="U",v3=test_pop)
+write.table(df1,"j_f3_analysis.ind",quote=F,col.names=F,row.names=F)
+
+test_pop2 <- as.vector(pops$V7[-c(1)])
+test_pop2 <- append(test_pop2,c("Jomon","Jomon","Jomon","Jomon","Jomon","Jomon","Kofun","Jomon","Kofun","Jomon","Kofun","Jomon","Jomon","Jomon"))
+df1 <- data.frame(v1=test_sum,v2="U",v3=test_pop2)
+write.table(df1,"j_f3_analysis2.ind",quote=F,col.names=F,row.names=F)
+
+
+for(ah in c("altai","chagyrskaya","vindija","denisova")){
+
+
+out <- file(sprintf("j_f3_analysis_%s.geno",ah),"w")
+out2 <- file(sprintf("j_f3_analysis_%s.snp",ah),"w")
+#set.seed(123)
+start.time<-proc.time()
+
+
+for(chrnum in 1:22){
+
+df <- read.csv(sprintf("chr%s-yri0%s1.csv",chrnum,ah))
+r <- df[,1]
+  
+  if(chrnum < 10){
+    f <- file(sprintf("j_merged0%s.txt",chrnum),"r")
+  }else{
+    f <- file(sprintf("j_merged%s.txt",chrnum),"r")
+  }
+  
+  repeat{
+    #reading data
+    v1 <- readLines(con=f,1)
+    v2 <- NULL
+    if(length(v1)==0){break}
+    
+    tmp <- as.vector(strsplit(v1,"\t")[[1]])
+    if((nchar(tmp[4])==1) && (nchar(tmp[5])==1)){
+      chr <- tmp[1]
+      pos <- tmp[2]
+
+if(pos %in% r == T){
+
+
+      id <- tmp[3]
+      ref <- tmp[4]
+      alt <- tmp[5]
+      v2 <- tmp[-c(1:6)]
+      
+      #changing format
+      n <- length(v2)
+      v3 <- NULL
+      for(i in 1:n){
+        if(v2[i]=="0|0" | v2[i]=="0/0" | v2[i]=="./." | v2[i]=="."){
+          v3 <- paste(v3,0,sep="")
+        }else{
+          if(v2[i]=="1|0" | v2[i]=="1/0" | v2[i]=="0|1" | v2[i]=="0/1"){
+            v3 <- paste(v3,1,sep="")
+          }else{
+            if(v2[i]=="1|1" | v2[i]=="1/1"){
+            v3 <- paste(v3,2,sep="")
+            }else{
+              v3 <- paste(v3,"MISS",sep="")
+            }
+          }
+        }
+      }
+      writeLines(v3, out, sep="\n")
+      v4 <- paste(id,chr,0,pos,ref,alt,sep=" ")
+      writeLines(v4, out2, sep="\n")
+
+}else{}
+
+    }else{}
+  }
+}
+
+end.time<-proc.time()
+end.time-start.time
+close(f)
+close(out)
+close(out2)
+
+}
