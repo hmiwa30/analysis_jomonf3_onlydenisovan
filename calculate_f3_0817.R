bcd1f021a47f614499f5a637c16feb842558109f
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2022-08-17 01:32:56 +0900

    [update] rename calculate_f3 to calculate_f3_0817

--- calculate_f3_0817.R
+++ calculate_f3_0817.R
@@ -15,20 +15,30 @@ library(admixtools)
 gc(reset=T)
 gc(reset=T)
 
-for(ah in c("altai","chagyrskaya","denisova","vindija")){
-  prefix = sprintf("G:\\hmiwa\\m_20211215\\05_jomonf3\\Geno\\Rename\\j_f3_analysis_%s",ah)
-  my_f2_dir = sprintf("G:\\hmiwa\\m_20211215\\05_jomonf3\\Results\\output_%s",ah)
-  extract_f2(prefix, my_f2_dir)
-  f2_blocks = f2_from_precomp(my_f2_dir)
-  dim(f2_blocks)
-  lista <- read.table("G:\\hmiwa\\m_20211215\\05_jomonf3\\Geno\\lista.txt")$V1
-  (pop1 = as.character(lista[1]))
-  (pop2 = as.character(lista[9]))
-  (pop3 = as.character(lista[-1]))
-  dat.f3=f3(f2_blocks,pop1,pop2,pop3)
-  write.table(dat.f3,sprintf("G:\\hmiwa\\m_20211215\\05_jomonf3\\Results\\f3_FIN_%s.txt",ah),
-              quote=F,row.names=F)
-}
+
+
+ah="onlydenisovan"
+
+#"05_jomonf3\\Results" -> "08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\results"
+#"05_jomonf3\\Geno\\Rename" -> "08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\sources"
+
+prefix = sprintf("G:\\hmiwa\\m_20211215\\05_jomonf3\\Geno\\Rename\\j_f3_analysis_%s",ah)
+
+my_f2_dir = sprintf("G:\\hmiwa\\m_20211215\\05_jomonf3\\Results\\output_%s",ah)
+extract_f2(prefix, my_f2_dir)
+f2_blocks = f2_from_precomp(my_f2_dir)
+dim(f2_blocks)
+lista <- read.table("G:\\hmiwa\\m_20211215\\05_jomonf3\\Geno\\lista.txt")$V1
+(pop1 = as.character(lista[1]))
+(pop2 = as.character(lista[9]))
+(pop3 = as.character(lista[-1]))
+dat.f3=f3(f2_blocks,pop1,pop2,pop3)
+write.table(dat.f3,sprintf("G:\\hmiwa\\m_20211215\\05_jomonf3\\Results\\f3_FIN_%s.txt",ah),
+            quote=F,row.names=F)
+
+
+
+
 
 for(ah in c("altai","chagyrskaya","denisova","vindija")){
   prefix = sprintf("G:\\hmiwa\\m_20211215\\05_jomonf3\\Geno\\Rename\\j_f3_analysis2_%s",ah)
