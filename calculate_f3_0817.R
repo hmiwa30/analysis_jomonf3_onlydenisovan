bc2a56aa961dbff3bcfcdd3e3b5c21cd702d4a79
hmiwa30 <hmiwa@g.ecc.u-tokyo.ac.jp>
2022-08-17 01:39:45 +0900

    Update calculate_f3_0817.R

--- calculate_f3_0817.R
+++ calculate_f3_0817.R
@@ -17,10 +17,19 @@ gc(reset=T)
 
 ah="onlydenisovan"
 
-prefix = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\sources\\j_f3_analysis_%s",ah)
+# extract_f2 ####
 
+prefix = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\sources\\j_f3_analysis_%s",ah)
 my_f2_dir = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\results\\output_%s",ah)
 extract_f2(prefix, my_f2_dir)
+
+prefix = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\sources\\j_f3_analysis2_%s",ah)
+my_f2_dir = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\results\\output2_%s",ah)
+extract_f2(prefix, my_f2_dir)
+
+# f2_blocks & f3 ####
+
+my_f2_dir = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\results\\output_%s",ah)
 f2_blocks = f2_from_precomp(my_f2_dir)
 dim(f2_blocks)
 lista <- read.table("G:\\hmiwa\\m_20211215\\05_jomonf3\\Geno\\lista.txt")$V1
@@ -31,9 +40,7 @@ dat.f3=f3(f2_blocks,pop1,pop2,pop3)
 write.table(dat.f3,sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\results\\f3_FIN_%s.txt",ah),
             quote=F,row.names=F)
 
-prefix = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\sources\\j_f3_analysis2_%s",ah)
 my_f2_dir = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\results\\output2_%s",ah)
-extract_f2(prefix, my_f2_dir)
 f2_blocks = f2_from_precomp(my_f2_dir)
 dim(f2_blocks)
 lista <- read.table("G:\\hmiwa\\m_20211215\\05_jomonf3\\Geno\\lista2.txt")$V1
@@ -44,10 +51,7 @@ dat.f3=f3(f2_blocks,pop1,pop2,pop3)
 write.table(dat.f3,sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\results\\f3_FIN2_%s.txt",ah),
             quote=F,row.names=F)
 
-
-#prefix = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\sources\\j_f3_analysis_%s",ah)
 my_f2_dir = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\results\\output_%s",ah)
-#extract_f2(prefix, my_f2_dir)
 f2_blocks = f2_from_precomp(my_f2_dir)
 dim(f2_blocks)
 lista <- read.table("G:\\hmiwa\\m_20211215\\05_jomonf3\\Geno\\lista.txt")$V1
@@ -58,9 +62,7 @@ dat.f3=f3(f2_blocks,pop1,pop2,pop3)
 write.table(dat.f3,sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\results\\f3_GBR_%s.txt",ah),
             quote=F,row.names=F)
 
-#prefix = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\sources\\j_f3_analysis2_%s",ah)
 my_f2_dir = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\results\\output2_%s",ah)
-#extract_f2(prefix, my_f2_dir)
 f2_blocks = f2_from_precomp(my_f2_dir)
 dim(f2_blocks)
 lista <- read.table("G:\\hmiwa\\m_20211215\\05_jomonf3\\Geno\\lista2.txt")$V1
@@ -71,9 +73,7 @@ dat.f3=f3(f2_blocks,pop1,pop2,pop3)
 write.table(dat.f3,sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\results\\f3_GBR2_%s.txt",ah),
             quote=F,row.names=F)
 
-#prefix = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\sources\\j_f3_analysis_%s",ah)
 my_f2_dir = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\results\\output_%s",ah)
-#extract_f2(prefix, my_f2_dir)
 f2_blocks = f2_from_precomp(my_f2_dir)
 dim(f2_blocks)
 lista <- read.table("G:\\hmiwa\\m_20211215\\05_jomonf3\\Geno\\lista.txt")$V1
@@ -84,9 +84,7 @@ dat.f3=f3(f2_blocks,pop1,pop2,pop3)
 write.table(dat.f3,sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\results\\f3_JPT_%s.txt",ah),
             quote=F,row.names=F)
 
-#prefix = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\sources\\j_f3_analysis2_%s",ah)
 my_f2_dir = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\results\\output2_%s",ah)
-#extract_f2(prefix, my_f2_dir)
 f2_blocks = f2_from_precomp(my_f2_dir)
 dim(f2_blocks)
 lista <- read.table("G:\\hmiwa\\m_20211215\\05_jomonf3\\Geno\\lista2.txt")$V1
@@ -98,9 +96,7 @@ write.table(dat.f3,sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf
             quote=F,row.names=F)
 
 
-#prefix = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\sources\\j_f3_analysis_%s",ah)
 my_f2_dir = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\results\\output_%s",ah)
-#extract_f2(prefix, my_f2_dir)
 f2_blocks = f2_from_precomp(my_f2_dir)
 dim(f2_blocks)
 lista <- read.table("G:\\hmiwa\\m_20211215\\05_jomonf3\\Geno\\lista.txt")$V1
@@ -111,9 +107,7 @@ dat.f3=f3(f2_blocks,pop1,pop2,pop3)
 write.table(dat.f3,sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\results\\f3_Jomon_%s.txt",ah),
             quote=F,row.names=F)
 
-#prefix = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\sources\\j_f3_analysis2_%s",ah)
 my_f2_dir = sprintf("G:\\hmiwa\\m_20211215\\08_jomonf3_2\\analysis_jomonf3_onlydenisovan\\results\\output2_%s",ah)
-#extract_f2(prefix, my_f2_dir)
 f2_blocks = f2_from_precomp(my_f2_dir)
 dim(f2_blocks)
 lista <- read.table("G:\\hmiwa\\m_20211215\\05_jomonf3\\Geno\\lista2.txt")$V1
